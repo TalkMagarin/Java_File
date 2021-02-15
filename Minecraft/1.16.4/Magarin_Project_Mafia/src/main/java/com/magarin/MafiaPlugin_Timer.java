94ad61a4acaac4c4c3342ba4e81d9046f31d87f7
Talk_Magarin <tkwoqhd@gmail.com>
2021-02-15 18:00:53 +0900

    2021-02-15 : 플레이어가 능력을 사용했을때 작동하는 클래스 생성

--- Minecraft/1.16.4/Magarin_Project_Mafia/src/main/java/com/magarin/MafiaPlugin_Timer.java
+++ Minecraft/1.16.4/Magarin_Project_Mafia/src/main/java/com/magarin/MafiaPlugin_Timer.java
@@ -3,6 +3,7 @@ package com.magarin;
 import com.magarin.files.File_Setting;
 import com.magarin.hashmap.Map_After_Start_Game;
 import com.magarin.hashmap.Map_Before_Start_Game;
+import com.magarin.hashmap.Map_Jobs_Skill;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -54,6 +55,7 @@ class Mafia_Before_Game_TimerTask extends TimerTask {
                     __Player__.sendMessage("Made in player - Talk_Magarin");
                     break;
                 case 4:
+                    __Player__.sendTitle("§60일차", "낮이 되었습니다. 첫날은 자유롭게 자기소개를 하며 토론하시기 바랍니다.");
                     this.cancel();
                     break;
             }
@@ -65,6 +67,7 @@ class Mafia_Before_Game_TimerTask extends TimerTask {
 class Mafia_After_Game_TimerTask extends TimerTask {
 
     boolean __Night__ = false;
+    boolean __Day__ = true;
 
     int __Day_Round__ = 0;
     int __Now_Times__ = 0;
@@ -74,42 +77,104 @@ class Mafia_After_Game_TimerTask extends TimerTask {
 
     public void run() {
         Map_After_Start_Game __Map_After_Start_Game = Map_After_Start_Game.__Instance__();
+        Map_Jobs_Skill __Map_Jobs_Skill = Map_Jobs_Skill.__Instance__();
 
         if (!__Night__) {
             // 낮일 경우
-            if (__Now_Times__ == 60) {
-                // 시간이 60초가 되었을때
-                if (__Day_Times__ > 0) {
-                    // 낮시간이 0보다 크면
-                    __Day_Times__--;
-                } else if (__Day_Times__ == 0) {
-                    // 낮시간이 0이면 투표시간으로 넘어감
-                    for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
-                        Player __Player__ = Bukkit.getPlayer(__Playername__);
-                        __Player__.sendTitle("§d투표 시간", "투표를 통해 마피아로 의심되는 사람을 지목하면 됩니다. 지목하지 않을 경우 스킵됩니다.");
-                    }
-                } else if (__Vote_Times__ > 0) {
-                    // 투표시간이 0보다 크면
-                    __Vote_Times__--;
-                }
-
-                __Now_Times__ = 0;
-            }
-
-            if (__Vote_Times__ == 0) {
-                // 투표시간이 0이면
-                __Day_Times__ = File_Setting.__get_Setting__Day_Time__();
-                __Vote_Times__ = File_Setting.__get_Setting__Vote_Time__();
-
-                __Night__ = true;
-
+            if (__Day_Times__ == 1) {
                 for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
                     Player __Player__ = Bukkit.getPlayer(__Playername__);
-                    __Player__.sendTitle("§d투표 결과", "투표를 통해 플레이어님이 처형당했습니다.");
+                    if (__Now_Times__ == 50) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 10초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 10초 남았습니다.");
+                    } else if (__Now_Times__ == 55) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 5초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 5초 남았습니다.");
+                    } else if (__Now_Times__ == 56) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 4초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 4초 남았습니다.");
+                    } else if (__Now_Times__ == 57) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 3초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 3초 남았습니다.");
+                    } else if (__Now_Times__ == 58) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 2초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 2초 남았습니다.");
+                    } else if (__Now_Times__ == 59) {
+                        __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 1초 남았습니다.");
+                        __Player__.sendMessage("마피아 투표 시간까지 1초 남았습니다.");
+                    }
                 }
+            }
+
+            if (__Now_Times__ == 60) {
+                // 시간이 60초가 되었을때
+                if (__Day__) {
+                    // 낮일 경우
+                    if (__Day_Times__ > 0) {
+                        // 낮시간이 0보다 크면
+                        __Day_Times__--;
+                    } else if (__Day_Times__ == 1) {
+                        for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+                            Player __Player__ = Bukkit.getPlayer(__Playername__);
+                            __Player__.sendTitle("§d시간 안내", "마피아 투표 시간까지 60초 남았습니다.");
+                            __Player__.sendMessage("마피아 투표 시간까지 60초 남았습니다.");
+                        }
+                    } else if (__Day_Times__ == 0) {
+                        // 낮시간이 0이면 투표시간으로 넘어감
+                        for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+                            Player __Player__ = Bukkit.getPlayer(__Playername__);
+                            __Player__.sendTitle("§d투표 시간", "투표를 통해 마피아로 의심되는 사람을 지목하면 됩니다. 지목하지 않을 경우 스킵됩니다.");
+                            __Player__.sendMessage("투표를 통해 마피아로 의심되는 사람을 지목하면 됩니다. 지목하지 않을 경우 스킵됩니다.");
+                        }
+
+                        __Day__ = false;
+                    }
+                } else {
+                    // 투표시간일 경우
+                    if (__Vote_Times__ > 0) {
+                        // 투표시간이 0보다 크면
+                        __Vote_Times__--;
+                    } else if (__Vote_Times__ == 0) {
+                        // 투표시간이 0이면
+                        __Day_Times__ = File_Setting.__get_Setting__Day_Time__();
+                        __Vote_Times__ = File_Setting.__get_Setting__Vote_Time__();
+
+                        __Night__ = true;
+
+                        for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+                            Player __Player__ = Bukkit.getPlayer(__Playername__);
+                            __Player__.sendTitle("§d투표 결과", "투표를 통해 플레이어님이 처형 당했습니다.");
+                            __Player__.sendMessage("투표를 통해 플레이어님이 처형 당했습니다.");
+                        }
+                    }
+                }
+
+                __Now_Times__ = -1;
             }
         } else {
             // 밤일 경우
+            if (__Map_Jobs_Skill.__Skill__Check_All_Player_Skill()) {
+                __Day_Round__++;
+                __Night__ = false;
+                __Day__ = true;
+
+                for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+                    Player __Player__ = Bukkit.getPlayer(__Playername__);
+                    __Player__.sendTitle(String.format("%s일차", __Day_Round__), "낮이 되었습니다.");
+                    __Player__.sendMessage("낮이 되었습니다.토론을 하여 마피아를 찾아내시기 바랍니다.");
+                }
+
+                __Now_Times__ = -1;
+            } else if (__Now_Times__ == 0) {
+                __Map_Jobs_Skill.__Skill__Reset_HashMap__();
+
+                for (String __Playername__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+                    Player __Player__ = Bukkit.getPlayer(__Playername__);
+                    __Player__.sendTitle("§7별이 빛나는 밤", "밤이 되었습니다. 각 직업군 들은 눙력을 사용해 주시기 바랍니다.");
+                    __Player__.sendMessage("밤이 되었습니다. 각 직업군 들은 눙력을 사용해 주시기 바랍니다.");
+                    __Player__.sendMessage("능력을 사용할 수 있는 모든 직업군들이 능력을 사용하지 않을 경우 라운드는 넘어가지 않습니다");
+                }
+            }
         }
 
         __Now_Times__++;
---
+++ Minecraft/1.16.4/Magarin_Project_Mafia/src/main/java/com/magarin/hashmap/Map_Jobs_Skill.java
@@ -0,0 +1,141 @@
+package com.magarin.hashmap;
+
+import com.magarin.files.File_Setting;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+<pre>
+플레이어가 직업군 스킬을 사용했을때 적용되는 클래스
+HashMap_Interface = Support Class
+</pre>
+ *
+ * 2021-02-15
+ * @author Talk_Magarin
+ */
+public class Map_Jobs_Skill implements HashMap_Interface {
+
+    private static Map_Jobs_Skill __Map_Jobs_Skill = null;
+    public static Map_Jobs_Skill __Instance__() {
+        if (__Map_Jobs_Skill == null)
+            __Map_Jobs_Skill = new Map_Jobs_Skill();
+        return __Map_Jobs_Skill;
+    }
+
+    private Map_After_Start_Game __Map_After_Start_Game = null;
+
+    // 플레이어가 게임에 참여하기 전 플레이어를 보관할 리스트
+    // String = Player name, String = Chief of the room player name
+    private HashMap<String, String> __Map_Jobs_Skill_List__ = null;
+
+    public Map_Jobs_Skill() {
+        this.__Map_Jobs_Skill_List__ = new HashMap<>();
+        __Map_After_Start_Game = Map_After_Start_Game.__Instance__();
+    }
+
+    public Integer __Skill__Jobs_Skill_List_Count() {
+        return this.__Map_Jobs_Skill_List__.size();
+    }
+
+    public boolean __Skill__Check_All_Player_Skill() {
+        for (String __Player_name__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+            String __get_Player_jobs__ = __Map_After_Start_Game.__Interface_Get_HashMap__(__Player_name__);
+            String __HashMap_Key__ = String.format("%s_%s", __Player_name__, __get_Player_jobs__);
+
+            if (__Interface_Get_HashMap__(__HashMap_Key__).equalsIgnoreCase("%%%null%%%"))
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 밤이 될때마다 사용할 스킬을 리셋할때 사용
+     */
+    public void __Skill__Reset_HashMap__() {
+        for (String __Player_name__ : __Map_After_Start_Game.__Interface_Get_HashMap_List__().keySet()) {
+            String __get_Player_jobs__ = __Map_After_Start_Game.__Interface_Get_HashMap__(__Player_name__);
+            String __HashMap_Key__ = String.format("%s_%s", __Player_name__, __get_Player_jobs__);
+
+            if (__Skill__Jobs_Skill_List_Count() == 0)
+                // Null = 스킬 사용전
+                this.__Map_Jobs_Skill_List__.put(__HashMap_Key__, "%%%null%%%");
+            else {
+                if (!__Interface_Get_HashMap__(__HashMap_Key__).equalsIgnoreCase("%%%die%%%")) {
+                    // 사용자가 죽지 않았을 경우 Null로 초기화
+                    this.__Map_Jobs_Skill_List__.replace(__HashMap_Key__, "%%%null%%%");
+                }
+            }
+        }
+    }
+
+    @Override
+    public HashMap<String, String> __Interface_Get_HashMap_List__() {
+        return __Map_Jobs_Skill_List__;
+    }
+
+    @Override
+    public String __Interface_Get_HashMap__(String __in_Player_name_And_Jobs) {
+        return this.__Map_Jobs_Skill_List__.get(__in_Player_name_And_Jobs);
+    }
+
+    @Override
+    public void __Interface_Set_HashMap__(String __in_Player_name, String __in_Select_Player_name) {
+        Player __Player__ = Bukkit.getPlayer(__in_Player_name);
+        String __get_Player_jobs__ = __Map_After_Start_Game.__Interface_Get_HashMap__(__in_Player_name);
+        String __HashMap_Key__ = String.format("%s_%s", __in_Player_name, __get_Player_jobs__);
+
+        if (__Interface_Get_HashMap__(__HashMap_Key__).equalsIgnoreCase("%%%null%%%")) {
+            if (__get_Player_jobs__.equalsIgnoreCase("Mafia")) {
+                boolean __Check_Skill__ = true;
+                __Player__.sendTitle(String.format("§c%s", __in_Select_Player_name), "처참하게 죽일 플레이어를 지목 하셨습니다.");
+                String[][] __Select_Kill_Players__ = new String[10][2];
+                Integer __Count__ = 0;
+                for (String __Player_Key__ : __Interface_Get_HashMap_List__().keySet()) {
+                    String[] __Player_Jobs__ = __Player_Key__.split("_");
+
+                    if (__Player_Jobs__[1].equalsIgnoreCase("Mafia")) {
+                        if (__Interface_Get_HashMap__(__Player_Key__).equalsIgnoreCase("%%%null%%%")) {
+                            __Check_Skill__ = false;
+                            break;
+                        } else {
+                            __Select_Kill_Players__[__Count__][0] = __Player_Jobs__[0];
+                            __Select_Kill_Players__[__Count__][1] = __Player_Jobs__[1];
+                        }
+                    }
+                }
+
+                if (__Check_Skill__) {
+                    for (String __Player_Key__ : __Interface_Get_HashMap_List__().keySet()) {
+                        String[] __Player_Jobs__ = __Player_Key__.split("_");
+
+                        if (__Player_Jobs__[1].equalsIgnoreCase("Mafia")) {
+
+                        }
+                    }
+                }
+            } else if (__get_Player_jobs__.equalsIgnoreCase("Doctor"))
+                __Player__.sendTitle(String.format("§a%s", __in_Select_Player_name), "해당 플레이어를 마피아의 공격으로 부터 치료 하였습니다.");
+            else if (__get_Player_jobs__.equalsIgnoreCase("Police"))
+                __Player__.sendTitle(String.format("§e%s", __in_Select_Player_name), "마피아 인지 확인할 플레이어를 지목 하셨습니다.");
+            else if (__get_Player_jobs__.equalsIgnoreCase("Spy"))
+                __Player__.sendTitle(String.format("§7%s", __in_Select_Player_name), "직업을 확인할 플레이어를 지목 하셨습니다.");
+            else if (__get_Player_jobs__.equalsIgnoreCase("Spiritualistic_Medium"))
+                __Player__.sendTitle(String.format("§8%s", __in_Select_Player_name), "죽은 플레이어의 직업을 확인할 플레이어를 지목 하셨습니다.");
+            else if (__get_Player_jobs__.equalsIgnoreCase("Detective"))
+                __Player__.sendTitle(String.format("§8%s", __in_Select_Player_name), "능력을 사용했는지 확인할 플레이어를 지목 하셨습니다.");
+            else {
+                __Player__.sendTitle("§4저런..", "당신은 능력을 사용할 수 없습니다.");
+                return;
+            }
+
+            this.__Map_Jobs_Skill_List__.replace(__HashMap_Key__, __in_Select_Player_name);
+        } else {
+            __Player__.sendTitle("§c에러", "이미 능력을 사용했습니다.");
+        }
+    }
+
+}
